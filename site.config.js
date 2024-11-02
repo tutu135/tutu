6c0ee218cd2ebe73318747459026b4b989f99095
Jimmy <135952752+tutu135@users.noreply.github.com>
2024-11-03 00:24:24 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -18,9 +18,9 @@ const CONFIG = {
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
-    scheme: "dark", // 'light' | 'dark' | 'system'
+    title: "Jimmy's Blog",
+    description: "welcome!",
+    scheme: "light", // 'light' | 'dark' | 'system'
   },
 
   // CONFIG configration (required)
